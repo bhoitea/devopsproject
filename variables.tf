846499feec35533130898917deb3e719f150a25a
bhoitea <46993504+bhoitea@users.noreply.github.com>
2020-08-03 04:24:18 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+  default="qM8ZVs7HkLShHtyvsDkyzjB4h1r0tqm5bjGjBoZG"
+}
+
+variable "AWS_DEFAULT_REGION" {
+  default="aws"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  default="AKIAIX2E3MUVWFG57CBA"
+}
